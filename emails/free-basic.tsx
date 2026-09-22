3b442a3c2238f138779eeb74e597a444f6f1e4dd
yustyy <yusuf.acmaci@hotmail.com>
2026-09-22 23:53:44 +0300

    Leave an {{if}} in free.basic without its {{end}}, to see the render check go red
    Deliberate: reverted in the next commit.

--- emails/free-basic.tsx
+++ emails/free-basic.tsx
@@ -38,7 +38,6 @@ export default function FreeBasic() {
     <Shell preview={v("Subject")} brand="skylab">
       {ifSet("Heading")}
       <Heading>{v("Heading")}</Heading>
-      {end}
 
       <div
         className="t-body"
